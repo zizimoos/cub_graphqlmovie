c4002ede7ce608e6e652ad3ed0be1f0f8447182c
zizimoos_iMac <zizimoos@gmail.com>
2020-11-14 18:16:16 +0900

    done

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
 import { GraphQLServer } from "graphql-yoga";
+import resolvers from "./graphql/resolvers";
 
-const typeDefs = `
-  type Query {
-    hello(name: String): String!
-  }
-`;
-
-const resolvers = {
-  Query: {
-    hello: (_, { name }) => `Hello ${name || "World"}`,
-  },
-};
-
-const server = new GraphQLServer({ typeDefs, resolvers });
+const server = new GraphQLServer({
+  typeDefs: "graphql/schema.graphql",
+  resolvers,
+});
 server.start(() => console.log("Server is running on localhost:4000"));
